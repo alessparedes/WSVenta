132c710b54d6711495ba691c9f5c4a4669c309b7
Alessandro Paredes <paredesandro@gmail.com>
2026-04-22 20:33:57 -0600

    feat(sales-logic): add data annotations and custom validation for sales
    - Implemented VentaRequest DTO with Required and Range annotations.
    - Added custom ExisteClienteAttribute validation using ValidationContext for DI.
    - Enhanced API security by ensuring only existing customers can place orders.
    - Initialized Conceptos list to prevent null reference exceptions.

--- WSVenta/Models/Request/VentaRequest.cs
+++ WSVenta/Models/Request/VentaRequest.cs
@@ -1,15 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WSVenta.Models.Request;
 
 public class VentaRequest
 {
+    [Required]
+    [Range(1, Double.MaxValue, ErrorMessage = "Valor de idCliente debe ser mayor a 0.")]
+    [ExisteCliente(ErrorMessage = "El cliente no existe.")]
     public int IdCliente { get; set; }
-    public decimal Total { get; set; }
-    public List<Concepto> Conceptos { get; set; }
-
-    public VentaRequest()
-    {
-        this.Conceptos = new List<Concepto>();
-    }
+    
+    [Required]
+    [MinLength(1, ErrorMessage = "Debe agregar al menos un concepto.")]
+    public List<Concepto> Conceptos { get; set; } = [];
 }
 
 public class Concepto
@@ -19,3 +21,19 @@ public class Concepto
     public decimal Importe { get; set; }
     public int IdProducto { get; set; }
 }
+
+#region Validaciones
+
+public class ExisteClienteAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var idcliente = Convert.ToInt32(value);
+        // Pedimos el DBContext al sistema de servicios de .NET
+        // Esto usa la configuración que ya tienes en Program.cs (Docker o Local)
+        var db = (VentaRealContext)validationContext.GetService(typeof(VentaRealContext))!;
+        
+        return db.Clientes.Any(c => c.Id == idcliente) ? ValidationResult.Success : new ValidationResult(ErrorMessage ?? "El cliente no existe.");
+    }
+}
+#endregion
